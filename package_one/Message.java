19ec8f124395719a67526aa27018a15287450cf7
Tamilarasan-mac <tamilarasanrajendran@outlook.com>
2025-12-23 15:30:28 +0530

    package_one file added

--- package_one/Message.java
+++ package_one/Message.java
@@ -1,16 +1,6 @@
 package package_one;
-
-public class Array {
-        public static void main(String[] args){
-        int[] marks = new int[10];
-
-        marks[0] = 69;
-        marks[1] = 89;
-        marks[2] = 95;
-
-        for (int i = 0; i < marks.length; i++) {
-            System.out.println("Mark: " + marks[i]);
-        }
+    public class Message {
+    public void showMessage() {
+        System.out.println("Hello from Package One");
     }
-    
 }
