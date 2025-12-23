b8f386db4140565851c81a5c319eae10675e49f3
Tamilarasan-mac <tamilarasanrajendran@outlook.com>
2025-12-23 14:46:03 +0530

    Modified Array2 filr

---
+++ package_two/Array2.java
@@ -0,0 +1,14 @@
+package package_two;
+
+public class Array2{
+	public static void main(String[] args){
+
+			int a = 7177;
+			int x = a;
+			int y = 8188;
+			int z = 11111;
+			double c = a/2d+y+z+11111.5d;
+		System.out.println(c);
+
+	}
+}
