bb49ca1f7f3fdb7bf0018019ddbf5ad90b9c74fb
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2025-12-25 14:31:26 +0530

    twodarray.java file added

--- twodarray.java
+++ twodarray.java
@@ -1,1 +1,14 @@
 
+public class twodarray {
+    public static void main(String[] args) {
+        int[][] a = {{10, 20, 30},
+                     {40, 50, 60},
+                     {70, 80, 90}};
+
+        System.out.println(a[0][1]);
+        System.out.println(a[2][2]);
+        System.out.println(a[2][1]);
+
+    }
+    
+}
