ea3fe5b9103354619e2fa220d6e67ec57a566666
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2025-12-28 23:13:16 +0530

    exten.java file added

---
+++ exten.java
@@ -0,0 +1,12 @@
+public class exten {
+        int x = 10;
+
+    }
+    class B  extends exten {
+        int x = 20;
+
+        void show() {
+            System.out.println(super.x);
+        }
+    }
+    
