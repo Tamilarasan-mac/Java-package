7bfea3849b8b205e16f91c0abeeec91a6cd203ec
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2025-12-30 10:15:16 +0530

    Arrayinit.java file added

---
+++ Arrayinit.java
@@ -0,0 +1,13 @@
+class Arrayinit {
+    public static void main(String[] args){
+        int[] marks = new int[10];
+
+        marks[0] = 69;
+        marks[1] = 89;
+        marks[2] = 95;
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println("Mark: " + marks[i]);
+        }
+    }
+}
