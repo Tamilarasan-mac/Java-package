ba8355b5f049334978c508718db863bf3629ae67
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2025-12-30 10:07:20 +0530

    tw.java file added

---
+++ tw.java
@@ -0,0 +1,12 @@
+public class tw{
+	public static void main(String[] args){
+
+			int a = 7177;
+			int x = a;
+			int y = 8188;
+			int z = 11111;
+			double c = a/2d+y+z+11111.5d;
+		System.out.println(c);
+
+	}
+}
