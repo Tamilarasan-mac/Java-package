405e1fbcc622abf398ec2b391956ec60bcd2a15f
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2026-01-03 11:30:44 +0530

    Modified user_get_index filr

--- user_get_index.java
+++ user_get_index.java
@@ -4,22 +4,23 @@ public class user_get_index {
     public static void main(String[] args) {
         int[] arr = {5, 15, 25, 35, 45};
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter value to search: ");
-        int search = sc.nextInt();
+      try  (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter value to search: ");
+            int search = sc.nextInt();
 
-        boolean found = false;
+            boolean found = false;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == search) {
-                System.out.println("Value found at index: " + i);
-                found = true;
-                break;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == search) {
+                    System.out.println("Value found at index: " + i);
+                    found = true;
+                    break;
+                }
             }
-        }
 
-        if (!found) {
-            System.out.println("Value not found");
+            if (!found) {
+                System.out.println("Value not found");
+            }
         }
     }
 }
