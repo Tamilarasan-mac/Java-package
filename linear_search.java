8dd72036cd27d09d268e7f401a60dc6590abfe4b
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2026-01-03 10:31:26 +0530

    linear_search.java file added

---
+++ linear_search.java
@@ -0,0 +1,9 @@
+public class linear_search {
+    public static void main(String[] args){
+        int[] get  = {10,100,1000,1001,101,111};
+        int value = get[5];
+
+    System.out.println("Value = " + value);
+        
+    }
+}
